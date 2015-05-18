1ec91a1686aaf003a368613eb88d7ae8714492d7
Reid Paape <wrpaape@gmail.com>
2015-05-17 20:48:25 -0500

    improved AI, not unbeatable yet

--- lib/computer.rb
+++ lib/computer.rb
@@ -13,13 +13,16 @@ class Computer
     current_token = @computer_token
     child_states = get_next_gen_states(@initial_state,current_token)
     child_scores = get_next_gen_scores(child_states,current_token)
+    puts "child_states"
+    puts child_states.inspect
     for index in 0...child_states.size
       if child_scores[index] == 0
         child_scores[index] = sum_leaf_scores(child_states[index],current_token)
       end
     end
 
-    puts child_scores
+    puts"child_scores"
+    puts child_scores.inspect
 
     best_move_index = 0
     for index in 1...child_scores.size
@@ -28,9 +31,14 @@ class Computer
       end
     end
 
+    initial_moves = get_current_token_moves(@initial_state,current_token)
     best_state = child_states[best_move_index]
     best_moves = get_current_token_moves(best_state,current_token)
-    best_move = best_moves.pop
+    if best_moves.size > 1
+      best_move = get_most_recent_move(initial_moves,best_moves)
+    else
+      best_move = best_moves.pop
+    end
     return best_move
   end
 
@@ -72,6 +80,17 @@ def get_next_gen_states(current_state,current_token)
     return current_token_moves
   end
 
+  def get_most_recent_move(previous_token_moves,current_token_moves)
+    most_recent_move = 0
+    current_token_moves.each do |move_index|
+      unless previous_token_moves.include?(move_index)
+        most_recent_move = move_index
+        break
+      end
+    end
+    return most_recent_move
+  end
+
   def switch_token(current_token)
     if current_token == "X"
       new_token = "O"
@@ -86,13 +105,16 @@ def get_next_gen_states(current_state,current_token)
     current_token = switch_token(current_token)
     unless @board.is_tie?(current_state)
       next_gen_states = get_next_gen_states(current_state,current_token)
-      puts "next_gen_states"
-      puts next_gen_states.inspect
+      # puts "next_gen_states"
+      # puts next_gen_states.inspect
       next_gen_scores = get_next_gen_scores(next_gen_states,current_token)
       for index in 0...next_gen_states.size
         if next_gen_scores[index] == 0
           next_gen_score = sum_leaf_scores(next_gen_states[index],current_token)
-          next_gen_scores.map! { |x| x == 0 ? next_gen_score : x }
+          # if next_gen_scores.index(0)
+          # next_gen_scores[next_gen_scores.index(0)] = next_gen_score
+          # end
+          next_gen_scores.map! { |x| x == 0 && next_gen_scores.index(x) == index ? next_gen_score : x }
         end
       end
       puts "end of branch gen scores"
